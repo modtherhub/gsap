fa11b21fa93a85ed9eecd89a4360e7c7c476f67c
modtherhub <moibhub@gmail.com>
2025-07-06 00:15:31 +0400

    create cards

--- src/App.tsx
+++ src/App.tsx
@@ -9,21 +9,21 @@ function App() {
 
   return (
     <>
-      <section className='card'>
-        <img className='card_bg' src={a} alt="image" />
-        <div className='card__blur'></div>
+      <section className='card relative py-[5rem] '>
+        <img className='card_bg    absolute top-0 left-0 w-[100%] h-[100%] object-center object-cover' src={a} alt="image" />
+        <div className='card__blur absolute top-0 left-0 w-[100%] h-[100%] backdrop-blur-[20px]'></div>
 
-        <div className='card__container container'>
-          <article className='card__artical'>
-            <img className='caed__img' src={y} alt="image" />
-            <div className='card__shadow '></div>
-
-            <div className='card-__data'>
-              <h2 className='card__name'></h2>
-              <span className='card__profession'></span>
+        <div className='card__container relative grid grid-cols-[260px] justify-center gap-[2rem]'>
+          <article className='card__artical relative rounded-[2rem] overflow-hidden'>
+            <img className='caed__img rounded-[2rem] transition-transform' src={y} alt="image" />
+            <div className='card__shadow absolute top-50 left-0 h-[100%] w-[100%] bg-gradient-to-b from-transparent to-black'></div>
+            
+            <div className='card-data text-white absolute left-[1.5rem] bottom-[2rem]'>
+              <h2 className='card__name font-bold text-2xl'>MODTHER</h2>
+              <span className='card__profession block text-sm'>DEV</span>
             </div>
 
-            <div className='card__clip'>
+            <div className='card__clip absolute'>
               <i class="ri-menu-5-fill"></i>
             </div>
           </article>
@@ -47,8 +47,8 @@ function App() {
             <div className='card__shadow '></div>
 
             <div className='card-__data'>
-              <h2 className='card__name'></h2>
-              <span className='card__profession'></span>
+              <h2 className='card__name'>MOHAMMED</h2>
+              <span className='card__profession'>DESIGNER</span>
             </div>
 
             <div className='card__clip'>
