638ecb1cc2c89a0f30fdf5ceffe84c8cce24ed71
modtherhub <moibhub@gmail.com>
2025-07-05 23:17:39 +0400

    create cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,62 @@
-import x from './assets/img-1.jpg'
-import y from './assets/img-2.jpg'
+import x from './assets/img-3.jpg'
+import y from './assets/img-4.jpg'
+import z from './assets/img-5.jpg'
+import a from './assets/img-6.jpg'
+
 
 function App() {
   
 
   return (
     <>
-      <nav>
-        <div>
-          <a href="#">Void Construct</a>
-        </div>
-        <div>
-          <p>Menu</p>
-          <p>Close</p>
-        </div>
-      </nav>
+      <section className='card'>
+        <img className='card_bg' src={a} alt="image" />
+        <div className='card__blur'></div>
 
-      <div className="container">
-        <div className="menu-contants">
-          <div className="menuitem">
-            <div className="col-lg">
-              <div className="menupreviewimg"><img src={x} alt="" /></div>
-            </div>
-            <div className="col-sm">
-              <div className="menu-links">
-                <div>
-                  <a href="#">Home</a>
-                </div>
-                <div>
-                  <a href="#">About</a> 
-                </div>
-                <div>
-                  <a href="#">Services</a>
-                </div>
-                <div>
-                  <a href="#">Projects</a>
-                </div>
-              </div>
-              <div className="menu-social">
-                <div>
-                  <a href="#">LinkedIn</a>
-                </div>
-                <div>
-                  <a href="#">YouTube</a>
-                </div>
-                <div>
-                  <a href="#">Instgram</a>
-                </div>
-                <div>
-                  <a href="#">Email</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          
-          <div className="menufotter">
-            <div>
-              <a href="#">Run Sequence</a>
-            </div>
-            <div>
-              <a href="#">Origin</a>
-              <a href="">Modther Abdalhag</a>
-            </div>
-          </div>
-        </div>
-      </div>
+        <div className='card__container container'>
+          <article className='card__artical'>
+            <img className='caed__img' src={y} alt="image" />
+            <div className='card__shadow '></div>
 
-      <div className='container'>
-        <section className='hero'>
-          <div><img src={y} alt="" /></div>
-          <h1>Welcome there I wish for you the best </h1>
-        </section>
-      </div>
+            <div className='card-__data'>
+              <h2 className='card__name'></h2>
+              <span className='card__profession'></span>
+            </div>
+
+            <div className='card__clip'>
+              <i class="ri-menu-5-fill"></i>
+            </div>
+          </article>
+
+          <article className='card__artical'>
+            <img className='caed__img' src={x} alt="image" />
+            <div className='card__shadow '></div>
+
+            <div className='card-__data'>
+              <h2 className='card__name'>ALI</h2>
+              <span className='card__profession'>Frontend Developer</span>
+            </div>
+
+            <div className='card__clip'>
+              <i class="ri-menu-5-fill"></i>
+            </div>
+          </article>
+
+          <article className='card__artical'>
+            <img className='caed__img' src={z} alt="image" />
+            <div className='card__shadow '></div>
+
+            <div className='card-__data'>
+              <h2 className='card__name'></h2>
+              <span className='card__profession'></span>
+            </div>
+
+            <div className='card__clip'>
+              <i class="ri-menu-5-fill"></i>
+            </div>
+          </article>
+        </div>
+      </section>
 
 
     </>
